23258bf284f8bfc2ac258f712d8844d0ac178c98
KS Edouard Song <me@ksesong.com>
2022-04-07 10:28:42 +0200

    style: format

--- build.rs
+++ build.rs
@@ -5,32 +5,32 @@ use std::path::PathBuf;
 use std::process::Command;
 
 fn get_output_path() -> PathBuf {
-  let manifest_dir_string = env::var("CARGO_MANIFEST_DIR").unwrap();
-  let build_type = env::var("PROFILE").unwrap();
-  let path = Path::new(&manifest_dir_string)
-    .join("target")
-    .join(build_type);
-  return PathBuf::from(path);
+    let manifest_dir_string = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let build_type = env::var("PROFILE").unwrap();
+    let path = Path::new(&manifest_dir_string)
+        .join("target")
+        .join(build_type);
+    return PathBuf::from(path);
 }
 
 fn main() {
-  let smc_dir = "src/external/smc-command";
-  let target_dir = get_output_path();
+    let smc_dir = "src/external/smc-command";
+    let target_dir = get_output_path();
 
-  Command::new("make").current_dir(&smc_dir).status().unwrap();
+    Command::new("make").current_dir(&smc_dir).status().unwrap();
 
-  fs::copy(
-    Path::new(&smc_dir).join("smc"),
-    Path::new(&target_dir).join("smc"),
-  )
-  .unwrap();
-
-  Command::new("make")
-    .arg("clean")
-    .current_dir(&smc_dir)
-    .status()
+    fs::copy(
+        Path::new(&smc_dir).join("smc"),
+        Path::new(&target_dir).join("smc"),
+    )
     .unwrap();
 
-  println!("cargo:rerun-if-changed=src/external/smc-command/smc.c");
-  println!("cargo:rerun-if-changed=src/external/smc-command/Makefile");
+    Command::new("make")
+        .arg("clean")
+        .current_dir(&smc_dir)
+        .status()
+        .unwrap();
+
+    println!("cargo:rerun-if-changed=src/external/smc-command/smc.c");
+    println!("cargo:rerun-if-changed=src/external/smc-command/Makefile");
 }
